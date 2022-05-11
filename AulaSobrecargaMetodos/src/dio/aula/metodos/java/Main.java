c9f51583695e178ff0c6a08377b8deb4b19e91b5
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-05-11 18:37:55 -0300

    Aula DIO sobre sobrecarga de métodos

---
+++ AulaSobrecargaMetodos/src/dio/aula/metodos/java/Main.java
@@ -0,0 +1,11 @@
+package dio.aula.metodos.java;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Exercício Quadrilátero:");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 6);
+        Quadrilatero.area(7, 5, 8);
+    }
+}
